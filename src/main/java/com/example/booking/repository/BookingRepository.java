7c7d951e96a48009b432723b03ce116de5561d64
Tumekie Blackwood <omarblackwood25@gmail.com>
2025-05-22 11:10:14 -0500

    created endpoint to get times for selected date

--- src/main/java/com/example/booking/repository/BookingRepository.java
+++ src/main/java/com/example/booking/repository/BookingRepository.java
@@ -3,8 +3,10 @@ package com.example.booking.repository;
 import com.example.booking.entity.Booking;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface BookingRepository extends JpaRepository<Booking, Long> {
     //List<Booking> findByUserId(Long userId);
+    List<Booking> findByDate(LocalDate date);
 }
